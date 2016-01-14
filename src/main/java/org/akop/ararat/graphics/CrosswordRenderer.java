5136afce9bbc5476b2a3df6b7dd59d919bc6f69e
Akop Karapetyan <dev@akop.org>
2016-01-14 00:49:03 -0800

    Moved more stuff into separate module

---
+++ src/main/java/org/akop/ararat/graphics/CrosswordRenderer.java
@@ -0,0 +1,107 @@
+// Copyright (c) 2014-2015 Akop Karapetyan
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package org.akop.ararat.graphics;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.DisplayMetrics;
+
+import org.akop.ararat.core.Crossword;
+
+
+public class CrosswordRenderer
+{
+	private static final int CELL_STROKE_COLOR = Color.parseColor("#000000");
+	private static final int CELL_FILL_COLOR = Color.parseColor("#ffffff");
+	private static final int PUZZLE_BG_COLOR = Color.parseColor("#000000");
+
+	private static final float CELL_STROKE_WIDTH = 1;
+
+	private Paint mCellStrokePaint;
+	private Paint mCellFillPaint;
+	private Paint mPuzzleBackgroundPaint;
+	private float mScaledCellStrokeWidth;
+
+	public CrosswordRenderer(Context context)
+	{
+		init(context.getResources());
+	}
+
+	private void init(Resources res)
+	{
+		DisplayMetrics dm = res.getDisplayMetrics();
+
+		mScaledCellStrokeWidth = CELL_STROKE_WIDTH * dm.density;
+
+		mCellStrokePaint = new Paint();
+		mCellStrokePaint.setColor(CELL_STROKE_COLOR);
+		mCellStrokePaint.setStyle(Paint.Style.STROKE);
+		mCellStrokePaint.setStrokeWidth(mScaledCellStrokeWidth);
+
+		mCellFillPaint = new Paint();
+		mCellFillPaint.setColor(CELL_FILL_COLOR);
+		mCellFillPaint.setStyle(Paint.Style.FILL);
+
+		mPuzzleBackgroundPaint = new Paint();
+		mPuzzleBackgroundPaint.setColor(PUZZLE_BG_COLOR);
+		mPuzzleBackgroundPaint.setStyle(Paint.Style.FILL);
+		mPuzzleBackgroundPaint.setStrokeWidth(mScaledCellStrokeWidth);
+	}
+
+	public void renderCrossword(Canvas canvas, Crossword crossword,
+			int wantedBmpWidth, int wantedBmpHeight)
+	{
+		int cwWidth = crossword.getWidth();
+		int cwHeight = crossword.getHeight();
+		int longestCwDim = Math.max(cwWidth, cwHeight);
+
+		float shortestWantedDim = Math.min(wantedBmpWidth, wantedBmpHeight);
+		float cellDim = (shortestWantedDim / longestCwDim)
+				- (mScaledCellStrokeWidth / longestCwDim);
+
+		float renderedWidth = cellDim * (float) cwWidth;
+		float renderedHeight = cellDim * (float) cwHeight;
+
+		Crossword.Cell[][] cellMap = crossword.getCellMap();
+
+		RectF puzzleRect = new RectF(0, 0, wantedBmpWidth, wantedBmpHeight);
+		canvas.drawRect(puzzleRect, mPuzzleBackgroundPaint);
+
+		RectF cellRect = new RectF();
+		float leftmost = ((float) wantedBmpWidth - renderedWidth) / 2f;
+		float rectTop = ((float) wantedBmpHeight - renderedHeight) / 2f;
+		for (int i = 0; i < cwHeight; i++, rectTop += cellDim) {
+			float rectLeft = leftmost;
+			for (int j = 0; j < cwWidth; j++, rectLeft += cellDim) {
+				if (cellMap[i][j] != null) {
+					cellRect.set(rectLeft, rectTop,
+							rectLeft + cellDim, rectTop + cellDim);
+					canvas.drawRect(cellRect, mCellFillPaint);
+					canvas.drawRect(cellRect, mCellStrokePaint);
+				}
+			}
+		}
+	}
+}
---
+++ src/main/java/org/akop/ararat/util/ReferenceScanner.java
@@ -0,0 +1,124 @@
+// Copyright (c) 2014-2015 Akop Karapetyan
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package org.akop.ararat.util;
+
+import org.akop.ararat.core.Crossword;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class ReferenceScanner
+{
+	private static final String WORD_ACROSS_EN = "Across";
+	private static final String WORD_DOWN_EN = "Down";
+
+	private static final Pattern NUMBER_FINDER = Pattern.compile("\\b(\\d{1,3})\\b");
+	private static final Pattern DIRECTION_FINDER_EN = Pattern.compile("((?:"
+			+ WORD_ACROSS_EN + ")|(?:" + WORD_DOWN_EN + "))",
+			Pattern.CASE_INSENSITIVE);
+
+	public static class WordReference
+	{
+		private int mNumber;
+		private int mDir;
+		private int mStart;
+		private int mEnd;
+
+		public int getNumber()
+		{
+			return mNumber;
+		}
+
+		public int getDirection()
+		{
+			return mDir;
+		}
+
+		public int getStart()
+		{
+			return mStart;
+		}
+
+		public int getEnd()
+		{
+			return mEnd;
+		}
+
+		@Override
+		public String toString()
+		{
+			String s = null;
+			if (mDir == Crossword.Word.DIR_ACROSS) {
+				s = mNumber + " Across";
+			} else if (mDir == Crossword.Word.DIR_DOWN) {
+				s = mNumber + " Down";
+			}
+
+			if (s != null) {
+				s += " [" + mStart + ".." + mEnd + "]";
+			} else {
+				s = "??";
+			}
+
+			return s;
+		}
+	}
+
+	public static List<WordReference> findReferences(Crossword.Word word,
+			Crossword crossword)
+	{
+		List<WordReference> refs = new ArrayList<>();
+
+		String hint = word.getHint();
+		Matcher m = NUMBER_FINDER.matcher(hint);
+		while (m.find()) {
+			// Find any numbers
+			int number = Integer.parseInt(m.group(1));
+			int start = m.start(1);
+			int end = m.end(1);
+
+			// Find closest directional marker
+			Matcher m2 = DIRECTION_FINDER_EN.matcher(hint);
+			if (m2.find(end)) {
+				int dir = WORD_ACROSS_EN.equalsIgnoreCase(m2.group(1))
+						? Crossword.Word.DIR_ACROSS
+						: Crossword.Word.DIR_DOWN;
+
+				// Confirm that the word exists in the crossword
+				Crossword.Word refWord = crossword.findWord(dir, number);
+				if (refWord != null) {
+					// It exists, add the reference
+					WordReference ref = new WordReference();
+					ref.mNumber = number;
+					ref.mDir = dir;
+					ref.mStart = start;
+					ref.mEnd = end;
+					refs.add(ref);
+				}
+			}
+		}
+
+		return refs;
+	}
+}
